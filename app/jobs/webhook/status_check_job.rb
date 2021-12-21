3e0dee2c2c67cbe936db6d630be522c31895c1d7
Dakhodl <dakhodl@protonmail.com>
2021-12-20 22:52:10 -0600

    dont catch connrefused error silently
    turns out this happens when config/initializers/tor.rb is not set to talk to tor container

--- app/jobs/webhook/status_check_job.rb
+++ app/jobs/webhook/status_check_job.rb
@@ -6,6 +6,5 @@ class Webhook::StatusCheckJob < ApplicationJob
     Rails.logger.info "Peer status: #{status.code} for #{peer.onion_address}"
     peer.update last_online_at: Time.current if status&.code == '200'
   rescue SOCKSError::HostUnreachable => e
-  rescue Errno::ECONNREFUSED => e
   end
 end
