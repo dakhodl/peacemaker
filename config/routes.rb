a9afe44a62433cf782e17d9f21f81e2e34b26475
Dakhodl <dakhodl@protonmail.com>
2021-12-27 18:14:36 -0600

    expose sidekiq webui unprotected for now
    started a death loop haha

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :ads
   resources :peers
@@ -10,6 +12,17 @@ Rails.application.routes.draw do
       get 'status', to: 'status#show'
     end
   end
+
+  # Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  #   # Protect against timing attacks:
+  #   # - See https://codahale.com/a-lesson-in-timing-attacks/
+  #   # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+  #   # - Use & (do not use &&) so that it doesn't short circuit.
+  #   # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+  #   ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+  #     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  # end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 
   root to: 'peers#index'
 end
