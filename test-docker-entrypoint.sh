6a7d100a2ce263b82585f8d3a715e42fdc67da54
Dakhodl <dakhodl@protonmail.com>
2021-12-31 15:13:22 -0600

    peers are running and talking to each other

--- test-docker-entrypoint.sh
+++ test-docker-entrypoint.sh
@@ -6,12 +6,18 @@ echo "Environment: $RAILS_ENV"
 
 
 # Remove pre-existing puma/passenger server.pid
-rm -f $APP_PATH/tmp/pids/server.pid
+rm -f $APP_PATH/tmp/pids/server-$INTEGRATION_SPECS.pid
+echo "deleted $APP_PATH/tmp/pids/server-$INTEGRATION_SPECS.pid"
 
 # Check if we need to install new gems
 bundle check || bundle install --jobs 20 --retry 5
 
+# systemctl start redis
 redis-server &
+
+bundle exec rake db:migrate
+
+ps -A
 
 # Then run any passed command
 bundle exec foreman s -f Procfile.integration
