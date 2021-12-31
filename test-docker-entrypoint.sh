b5c610045b877a5e58d71fb10a53ba9dc1a313cc
dakhodl <dakhodl@protonmail.com>
2021-12-30 22:02:54 -0600

    add basic test container stack
    can I get one container running the app
    then another one running capybara
    then another one running the app, and another, and another
    
    and drive capybara specs across various peers to test the
    peer to peer, end to end behavior
    
    step 1 worked out, onto the next one

--- test-docker-entrypoint.sh
+++ test-docker-entrypoint.sh
@@ -4,8 +4,14 @@ set -e
 
 echo "Environment: $RAILS_ENV"
 
+
+# Remove pre-existing puma/passenger server.pid
+rm -f $APP_PATH/tmp/pids/server.pid
+
 # Check if we need to install new gems
 bundle check || bundle install --jobs 20 --retry 5
 
+redis-server &
+
 # Then run any passed command
-bundle exec ${@}
+bundle exec foreman s -f Procfile.integration
