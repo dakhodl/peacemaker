978a59636ce28bdbd55b4fbddc7c226368e68072
Dakhodl <dakhodl@protonmail.com>
2022-01-05 22:24:00 -0600

    remove webpack, upgrade tailwind, install importamp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def nav_link_to(link_path, path_controller_name, &block)
-    current_page_style = controller_name == path_controller_name ? 'border-indigo-500 text-gray-900' : ''
+    current_page_style = controller_name == path_controller_name ? 'border-indigo-500 text-gray-900 border-b-2' : 'border-transparent text-gray-500'
     link_to link_path,
-            class: "#{current_page_style} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium border-transparent border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700", &block
+            class: "#{current_page_style} inline-flex items-center px-1 pt-1 text-sm font-medium hover:border-b-2 hover:border-gray-300 hover:text-gray-700", &block            
   end
 end
