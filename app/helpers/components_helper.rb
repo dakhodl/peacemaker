fd5974ad378629a9cb6e4b33a10a87bd4c590abc
Dakhodl <dakhodl@protonmail.com>
2022-01-17 10:01:33 -0600

    rename Message nav to Chat and swap out some icons, onboarding polish

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -1,6 +1,6 @@
 module ComponentsHelper
   def empty_state_container
-    content_tag(:div, class: "w-full h-full md:mt-36 lg:mt-0 flex flex-col items-center justify-center content-center") do
+    content_tag(:div, class: "w-full h-full mt-24 md:mt-36 lg:mt-0 flex flex-col items-center justify-center content-center") do
       yield
     end
   end
--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -16,4 +16,20 @@ SVG
 </svg>
 SVG
   end
+
+  def icon_empty_inbox
+    <<-SVG.strip.html_safe
+<svg xmlns="http://www.w3.org/2000/svg" class="h-44 w-44" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+</svg>
+SVG
+  end
+
+  def icon_chat
+    <<-SVG.strip.html_safe
+<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+</svg>
+SVG
+  end
 end
