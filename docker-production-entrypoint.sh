386ee413831445733ef6e087376d0689a0755566
Dakhodl <dakhodl@protonmail.com>
2022-02-06 23:15:03 -0600

    working production image builds :tada:

--- docker-production-entrypoint.sh
+++ docker-production-entrypoint.sh
@@ -4,14 +4,11 @@ set -e
 
 echo "Environment: $RAILS_ENV"
 
-echo "What is in this directory?"
-ls -l
-
 # Remove pre-existing puma/passenger server.pid
 rm -f $APP_PATH/tmp/pids/server.pid
 
 # ensure hidden service folder has correct perms for tor
-# chmod -R 700 /var/app/hidden_service/
+chmod -R 700 /var/app/hidden_service/
 
 # Generate a new hs_ed25519_secret_key, hs_ed5519_public_key, and hostname if both were not specified
 if [ ! -f "${APP_PATH}/hidden_service/hs_ed25519_secret_key" ] || [ ! -f "${APP_PATH}/hidden_service/hs_ed25519_public_key" ] || [ ! -f "${APP_PATH}/hidden_service/hostname" ]; then
--- docker-tor-entrypoint.sh
+++ docker-tor-entrypoint.sh
@@ -10,7 +10,5 @@ echo "[TOR] Starting onion service at ${TOR_HOSTNAME}"
 echo "[TOR] ==================================================================="
 echo ""
 
-chown root -R hidden_service/
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
