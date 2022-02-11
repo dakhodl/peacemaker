e046f48b4cbbfcc26307a3f3b392c068fa242f4b
dakhodl <dakhodl@protonmail.com>
2022-02-11 09:47:16 -0600

    generate signing keys on boot
    a step toward using the tor key files
    which I still can't figure out how to read and pass to RbNaCl
    
    This struck me as a great step though,
    in that we no longer need a two request send <-> fetch webhook cycle.
    
    We can just start signing requests on the fly as hoped.

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -5,3 +5,15 @@
 #   configatron.emails.sales_reciept.subject = 'Thanks for your order'
 #
 #   configatron.file.storage = :s3
+
+if File.exists?("hidden_service/peacemaker_key")
+  secret_key = RbNaCl::PrivateKey.new File.read('hidden_service/peacemaker_key').b
+else
+  secret_key = RbNaCl::PrivateKey.generate
+  File.open('hidden_service/peacemaker_key', 'wb') do |f|
+    f.write(secret_key.to_s.b)
+  end
+end
+
+configatron.public_key = secret_key.public_key
+configatron.secret_key = secret_key
