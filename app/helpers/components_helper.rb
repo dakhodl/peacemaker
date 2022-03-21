99a063df02aa421896d11ddcecc5f22456ffc0f3
dakhodl <dakhodl@protonmail.com>
2022-03-21 09:23:44 -0500

    add some peer metrics to /peers

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -24,4 +24,14 @@ module ComponentsHelper
       yield if block_given?
     end
   end
+
+  def change_percentage_bubble(change)
+    content_tag(:div, class: "inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0") do
+      concat arrow_sm_up
+      concat(content_tag(:span, class: "sr-only") do
+        'Increased by'
+      end)
+      concat number_to_percentage change, precision: 0
+    end
+  end
 end
